55eee9b83278a7435dd8083aed977fa882cfa870
Marc <marcjfj@gmail.com>
2019-10-19 13:10:55 -0500

    added google maps and truck listings

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,26 @@
+$('#food-style-drop').on('click', function() {
+  $('#food-style-modal').css('display', 'block');
+});
 
-$('#food-style-drop').on('click', function(){
-    $('#food-style-modal').css('display', 'block');
-})
+$('.closeBtn').on('click', function() {
+  $('#food-style-modal').css('display', 'none');
+});
 
-$('.closeBtn').on('click', function(){
+$(window).on('click', function(event) {
+  if (event.target.id == 'food-style-modal') {
     $('#food-style-modal').css('display', 'none');
-})
+  }
+});
+$('#distance-drop').on('click', function() {
+  $('#food-style-modal').css('display', 'block');
+});
 
-$(window).on('click', function(event){
-    if (event.target.id == 'food-style-modal'){
-        $('#food-style-modal').css('display', 'none');
-    }
-})
-$('#distance-drop').on('click', function(){
-    $('#food-style-modal').css('display', 'block');
-})
+$('.closeBtn').on('click', function() {
+  $('#radius-modal').css('display', 'none');
+});
 
-$('.closeBtn').on('click', function(){
+$(window).on('click', function(event) {
+  if (event.target.id == 'radius-modal') {
     $('#radius-modal').css('display', 'none');
-})
-
-$(window).on('click', function(event){
-    if (event.target.id == 'radius-modal'){
-        $('#radius-modal').css('display', 'none');
-    }
-})
+  }
+});
